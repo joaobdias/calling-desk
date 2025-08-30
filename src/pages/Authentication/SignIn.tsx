e989d6f522db143df3e20f3fe3b10a51112879dc
joaobdias <joaobdiasbr@gmail.com>
2025-08-30 14:18:52 -0300

    refactor: remove InputBase and InputSimple to keep in the page

--- src/pages/Authentication/SignIn.tsx
+++ src/pages/Authentication/SignIn.tsx
@@ -1,9 +1,14 @@
 import image from "../../assets/image.jpg";
 import { Logo } from "../../components/Logo";
-import { InputBase } from "../../components/InputBase";
-import { InputBaseSimple } from "../../components/InputSimple";
+import { TitleField } from "../../components/TitleField";
+import { Input } from "../../components/Input";
+import { ButtonBlack } from "../../components/ButtonBlack";
+import { ButtonGray } from "../../components/ButtonGray";
+import { useNavigate } from "react-router";
 
 export function SignIn() {
+  const nav = useNavigate();
+
   return (
     <div>
       <img
@@ -16,8 +21,27 @@ export function SignIn() {
         <div className="flex justify-center mt-16">
           <Logo />
         </div>
-        <InputBase />
-        <InputBaseSimple />
+        <div className="flex flex-col border-1 border-gray-200 rounded-2xl mt-8 ml-36 mr-36 p-8">
+          <TitleField
+            title="Acesse o Portal"
+            description="Entre usando seu e-mail e senha"
+          />
+          <div className="mt-12 mb-8">
+            <Input title="E-MAIL" placeholder="exemplo@email.com" />
+            <Input title="SENHA" placeholder="senha" />
+          </div>
+          <ButtonBlack textButton="Entrar" />
+        </div>
+        <div className="flex flex-col border-1 border-gray-200 rounded-2xl mt-8 ml-36 mr-36 p-8">
+          <TitleField
+            title="Ainda sem conta?"
+            description="Crie agora mesmo!"
+          />
+          <ButtonGray
+            textButton="Criar conta"
+            onClick={() => nav("/criar-conta")}
+          />
+        </div>
       </div>
     </div>
   );
